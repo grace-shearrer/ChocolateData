bfdd42805f1224105dd5360c1e048ad5193a1c13
NikkiBytes <nicholletteacosta@gmail.com>
2019-03-06 20:50:34 -0500

    added design2 file

---
+++ preprocessing/feat2/.ipynb_checkpoints/design2_generator-checkpoint.py
@@ -0,0 +1,36 @@
+import glob
+import os
+DER_DIR = "/projects/niblab/bids_projects/Experiments/ChocoData/derivatives"
+SUB_DIR = "/projects/niblab/bids_projects/Experiments/ChocoData/derivatives/sub-*/ses-1"
+SUB_DIRS = glob.glob(SUB_DIR)
+
+for sub in SUB_DIRS:
+    subject = sub.split("/")[-2]
+    print("SUBJECT: ", subject)
+    #check for existence of feat2 directory
+    FEAT2_DIR = os.path.join(sub, "func/Analysis/feat2")
+    if os.path.exists(FEAT2_DIR):
+        pass
+    else:
+        os.makedirs(FEAT2_DIR)
+    print("> STARTING PROGRAM......")
+    print("----------------------->>>>SUBJECT: ",sub)
+    FEATS_PATH = os.path.join(sub, "func/Analysis/feat1/*.feat")
+    FEATS = glob.glob(FEATS_PATH)
+    with open(os.path.join(DER_DIR, "design2.fsf"), 'r') as infile:
+        tempfsf=infile.read()
+        # set outpath for fsf OUTPATH variable, by run
+        subID =  sub.split("/")[-1]
+        outpath = os.path.join(sub, "func/Analysis/feat2", subID)
+        print(">>>>>>>>>>>>>>>>>SETTING DESIGN OUTPATH: ", outpath)
+        #tempfsf = tempfsf.replace("OUTPUT", outpath)
+        print(FEATS)
+        for index, feat_path in enumerate(FEATS):
+            feat_id = "FEAT%s"%(index+1)
+            tempfsf = tempfsf.replace(feat_id, feat_path)
+        OUTFILE_PATH = os.path.join(FEAT2_DIR, "%s_design.fsf"%subject)
+        print("OUTFILE ------------------------>>>> ", OUTFILE_PATH)
+        with open(OUTFILE_PATH, "w") as outfile:
+            outfile.write(tempfsf)
+        outfile.close()
+    infile.close()
